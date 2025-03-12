01376517360f476f16d43d99dbb7fc0989598b79
nseon <nseon@student.42lyon.fr>
2025-03-12 11:32:06 +0100

    fix: rout & routapp in check_op

--- src/commands/operands.c
+++ src/commands/operands.c
@@ -6,7 +6,7 @@
 /*   By: nseon <nseon@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 08:40:45 by nseon             #+#    #+#             */
-/*   Updated: 2025/03/12 11:16:03 by nseon            ###   ########.fr       */
+/*   Updated: 2025/03/12 11:28:33 by nseon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void check_op(t_cmd *cmd)
 
 	if (cmd->output.op == ROUT)
 	{
-		fd = open(cmd->output.path, O_WRONLY);
+		fd = open(cmd->output.path, O_WRONLY | O_TRUNC);
 		dup2(fd, 1);
 	}
 	if (cmd->output.op == ROUTAPP)
