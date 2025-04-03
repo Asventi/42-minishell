7e162a4678470e8a7e6c9e35ee0f73bdf40c0e0b
Pierre Jarnac <pjarnac@student.42lyon.fr>
2025-04-03 16:52:44 +0200

    onrecommitcarilfaitnimp

--- src/commands/exec.c
+++ src/commands/exec.c
@@ -46,7 +46,7 @@ int32_t	exec_builtin(t_cmd *cmd, t_context *ctx,
 			close(pipefd[0]);
 		if (check_op(cmd) == -1 || launch_builtins(cmd, ctx) == -1)
 			return (close(pipefd[1]), CHLD_ERR);
-		close(pipefd[1]);
+		close_in_out(fdin, pipefd[1]);
 		return (EXIT * (!ft_strcmp(cmd->path, "exit")) + CHLD_END
 			* (ft_strcmp(cmd->path, "exit") != 0));
 	}
